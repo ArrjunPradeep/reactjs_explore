d9cc5077f2fb410300f47a9cf7ad092922c17ee4
Arju <iaat@pop-os.localdomain>
2021-05-18 19:52:46 +0530

    mount_unmount_update

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Counter() {
+
+    const [count,setCount] = useState(0);
+
     return (
         <div>
-            <h1>Hello I am Component</h1>
+            <button onClick={()=>setCount(count+1)} >Increment</button>
+            <h1>Hello I am Component : {count}</h1>
         </div>
     )
 }
